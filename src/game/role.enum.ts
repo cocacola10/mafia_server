07936c36817f2788921c4df1577ff2d55804d1ac
cocacola10 <hg9509198@gmail.com>
2024-02-27 10:42:35 +0900

    Added game.service.ts and fixed shufflePlayers method

---
+++ src/game/role.enum.ts
@@ -0,0 +1,10 @@
+// src/game/role.enum.ts
+
+export enum Role {
+    Mafia = "Mafia",
+    Citizen = "Citizen",
+    Doctor = "Doctor",
+    Police = "Police",
+    // Add other roles if necessary
+  }
+  
